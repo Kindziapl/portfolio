8b0e6e5bf5a655442bd0f76e95334dd84b037ffc
KDudaDeweloperskie <katarzyna.duda@deweloperskie.pl>
2025-08-08 15:15:51 +0200

    shvfqhjs

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <>
+      <div className="w-full h-[100svh] flex flex-row items-center bg-background font-heading font-size-3xl">
+        <div className="w-1/5 h-full bg-primary"></div>
+        <div className="w-3/5 h-full bg-background text-txt text-4xl">
+          <div className="card"> Moja stronka UwU </div>
+        </div>
+        <div className="w-1/5 h-full bg-primary"></div>
+      </div>
+    </>
+  );
+}
