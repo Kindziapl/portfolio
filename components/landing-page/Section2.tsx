f4a0a88816695df22eed754b40c2d5a9697dceb5
KindziaplHome <kindziapl@interia.pl>
2025-08-13 19:06:24 +0200

    add pictures

--- components/landing-page/Section2.tsx
+++ components/landing-page/Section2.tsx
@@ -23,7 +23,7 @@ const mojeinfo: InfoType[] = [
     date: "2021",
     materials: "Eva foam, textyles, faux-fur, worbla",
     techniques: "Sewing, foam carving, airbrush painting, 3d&nbsp;modeling",
-    imgSrc: "/assets/pictures/xayah.jpg",
+    imgSrc: "/assets/pictures/hiryuu-pic-1.jpg",
     visualInfluence: [
       "/assets/pictures/hiryuu-ref-1.jpg",
       "/assets/pictures/hiryuu-ref-2.jpg",
@@ -37,7 +37,7 @@ const mojeinfo: InfoType[] = [
     date: "2019",
     materials: "Eva foam, textyles",
     techniques: "Sewing, foam carving, airbrush painting",
-    imgSrc: "/assets/pictures/xayah.jpg",
+    imgSrc: "/assets/pictures/sivir-pic-1.jpg",
     visualInfluence: [
       "/assets/pictures/sivir-ref-1.jpg",
       "/assets/pictures/sivir-ref-2.jpg",
@@ -51,7 +51,7 @@ const mojeinfo: InfoType[] = [
     date: "2022",
     materials: "Eva foam, textyles, PLA",
     techniques: "Sewing, foam carving, 3d modeling, wig&nbsp;styling",
-    imgSrc: "/assets/pictures/xayah.jpg",
+    imgSrc: "/assets/pictures/richelieu-pic-1.jpg",
     visualInfluence: [
       "/assets/pictures/richelieu-ref-1.jpg",
       "/assets/pictures/richelieu-ref-2.jpg",
@@ -65,7 +65,7 @@ const mojeinfo: InfoType[] = [
     date: "2025",
     materials: "Eva foam, textyles, PLA, faux fur",
     techniques: "Sewing, foam carving, 3d modeling, wig&nbsp;styling, corset ",
-    imgSrc: "/assets/pictures/xayah.jpg",
+    imgSrc: "/assets/pictures/pestonya-pic-1.jpg",
     visualInfluence: [
       "/assets/pictures/pestonya-ref-1.jpg",
       "/assets/pictures/pestonya-ref-2.jpg",
