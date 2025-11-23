9a8c499bfd666693688ced6925300ebd23f1234e
Kindziapl <kindziapl@interia.pl>
2025-11-23 22:46:55 +0100

    idk ;-;

--- next.config.ts
+++ next.config.ts
@@ -4,8 +4,8 @@ const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: isProd ? "/portfolio" : "",
-  assetPrefix: isProd ? "/portfolio/" : "",
+  // basePath: isProd ? "/portfolio" : "",
+  // assetPrefix: isProd ? "/portfolio/" : "",
   images: {
     unoptimized: true,
   },
