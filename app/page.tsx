66323f9c30c800f3d33f795edad490f3af27facc
Kindziapl <kindziapl@interia.pl>
2025-11-23 19:54:57 +0100

    add text to secion1

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,11 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex flex-col gap-0">
+    <main className="flex flex-col relative gap-0">
+      <div className="absolute top-0 right-0 z-10 w-full h-10 text-2xl text-white">
+        {" "}
+        Work in progress
+      </div>
       <Section1 />
 
       <div className="w-full h-[100px] flex bg-secondary text-4xl text-txt items-center px-20">
